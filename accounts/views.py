a8c3947ac016e339f2f0952baf4d503088a93a41
Jaccorot <caozijun007@163.com>
2014-05-15 00:46:40 +0800

    增加一些基本的登录相关信息

---
+++ accounts/views.py
@@ -0,0 +1,27 @@
+#!-*- coding=utf-8 -*-
+
+from django.shortcuts import render
+
+'''
+登录索引页
+'''templatesaccounts
+def index(request):  
+    pass  
+
+'''
+用户注册页
+'''  
+def register(request):  
+    pass  
+ 
+'''
+用户登录页
+'''
+def login(request):  
+    pass  
+ 
+'''
+用户登出页
+'''
+def logout(request):  
+    pass  
