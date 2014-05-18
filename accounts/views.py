dd317e64cfc966d8d19e7fb01ced7d95818415e0
ddkangfu <ddkangfu@gmail.com>
2014-05-19 01:02:29 +0800

    增加了登录相关的代码，为主页增加了用户是否登录的检查。

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,27 @@
 #!/usr/bin/python
 
 from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+from django.contrib import auth
 
 
 def login(request):  
-    return render_to_response("accounts/login1.html")
+	err_msg = ''
+	
+	if request.method == 'POST':
+		username = request.POST.get('username', '')
+		password = request.POST.get('password', '')
+		next_url = request.POST.get('next', '')
+		is_remberme = request.POST.get('remberme', False)
+		user = auth.authenticate(username=username, password=password)
+		if user is not None and user.is_active:
+			auth.login(request, user)
+			if next_url:
+				return HttpResponseRedirect(next_url)
+			return HttpResponseRedirect("/")
+		else:
+			return HttpResponseRedirect("/account/invalid/")
+	return render_to_response("accounts/login.html", {"err_msg": err_msg})
  
 '''
 用户登出页
