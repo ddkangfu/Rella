45fa5b4e593ca9e46c809c3b04d6911de124d3f3
ddkangfu <ddkangfu@gmail.com>
2014-11-01 00:05:18 +0800

    增加了测试accounts app的测试方法框架。

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+class test_login_view(TestCase):
+    def __init__(self):
+        pass
+
+    def test_login_with_get_method(self):
+        pass
+
+    def test_login_with_no_username(self):
+        pass
+
+    def test_login_with_no_password(self):
+        pass
+
+
+class test_logout_view(TestCase):
+    def __init__(self):
+        pass
+
+    def test_logout_success(self):
+        pass
